78a12fcfa96c1776571ef99f08bc5ad1841e9fa0
UlisesVeraC <veracuevasu@gmail.com>
2025-04-22 10:07:43 -0600

    Regions Done

--- src/app/country/pages/country-menus/country-menus.component.ts
+++ src/app/country/pages/country-menus/country-menus.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-country-menus',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-menus.component.html',
 })
 export class CountryMenusComponent {
 
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+  regions = signal(this.countryService.getRegions());
+  countriesByRegion = signal<Country[]>([]);
+  bordersByCountry = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region:['', Validators.required],
+    country:['', Validators.required],
+    border:['', Validators.required],
+  })
 }
